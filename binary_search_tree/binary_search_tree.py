0577749b6287d845f3d733e96dc35b56fdd1cc23
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-05-13 17:50:47 -0400

    Completed insert method on binary search tree.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,13 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value and self.left is None:
+      self.left = BinarySearchTree(value)
+    elif value >= self.value and self.right is None:
+      self.right = BinarySearchTree(value)
+    else:
+      branch = self.left if value < self.value else self.right
+      branch.insert(value)
 
   def contains(self, target):
     pass
@@ -15,3 +21,13 @@ class BinarySearchTree:
 
   def for_each(self, cb):
     pass
+
+
+bst = BinarySearchTree(5)
+
+bst.insert(2)
+bst.insert(3)
+bst.insert(7)
+bst.insert(6)
+print(bst.left.right.value == 3)
+print(bst.right.left.value == 6)
