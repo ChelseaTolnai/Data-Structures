bdf93c331c6a8b4e56f84c7a2bcfc08a4f67eea1
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-05-15 17:55:23 -0400

    Stretch - Completed LRU Cache file. Tests pass.

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,6 +1,10 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
   def __init__(self, limit=10):
-    pass
+    self.limit = limit
+    self.length = 0
+    self.doubly_linked_list = DoublyLinkedList()
 
   """
   Retrieves the value associated with the given key. Also
@@ -10,7 +14,10 @@ class LRUCache:
   key-value pair doesn't exist in the cache. 
   """
   def get(self, key):
-    pass
+    exists = self.doubly_linked_list.get_item(key)
+    if exists:
+      self.doubly_linked_list.move_to_front(exists)
+      return exists.value[key]
 
   """
   Adds the given key-value pair to the cache. The newly-
@@ -23,4 +30,13 @@ class LRUCache:
   the newly-specified value. 
   """
   def set(self, key, value):
-    pass
+    exists = self.doubly_linked_list.get_item(key)
+    if exists:
+      exists.value[key] = value
+      self.doubly_linked_list.move_to_front(exists)
+    else:
+      self.doubly_linked_list.add_to_head({key: value})
+      self.length += 1
+      if self.length > self.limit:
+        self.doubly_linked_list.remove_from_tail()
+        
